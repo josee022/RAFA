3f48106cad84db4b6f9684e07212c4a8b8ff24ce
jmondelo <jose.mondelo@iesdonana.org>
2024-02-20 21:21:04 +0100

    reactttt

--- U08/carrito/src/components/Footer.jsx
+++ U08/carrito/src/components/Footer.jsx
@@ -3,7 +3,34 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      {/* Codigo del pie de la web */}
+        <hr class="lineaDorada" />
+        <div>
+            <div>
+                <h2 class="ayudah2">AYUDA</h2><br/><br/>
+                <p class="terminos">Términos y condiciones</p><br/><br/>
+                <p class="terminos">Política de privacidad</p><br/><br/>
+                <p class="terminos">Política de cookies</p><br/><br/>
+                <p class="terminos">Preguntas frecuentes</p><br/><br/>
+            </div>
+
+            <div>
+                <h2 class="contactah2">CONTACTA CON NOSOTROS</h2><br/><br/>
+                <p class="terminos2"><img class="gmail" src="imagenes/gmail.png" alt="gmail" />&nbsp Gym.mondelo@gmail.com
+                </p><br/><br/>
+                <p class="terminos2"><img class="gmail" src="imagenes/telefono.png" alt="gmail" />&nbsp 622 33 18 27</p>
+                <br/><br/>
+                <p class="terminos2"><img class="gmail" src="imagenes/instagram.png" alt="gmail" />&nbsp @GymMondelo_</p>
+                <br/><br/>
+                <p class="terminos2"><img class="gmail" src="imagenes/youtube.png" alt="gmail" />&nbsp GymMondelo Fit</p>
+                <br/><br/>
+            </div>
+        </div>
+        <div>
+            <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">
+                <img class="licencia" src="https://licensebuttons.net/l/by/4.0/88x31.png"
+                    alt="Creative Commons License" />
+            </a>
+        </div>
     </footer>
   );
 };
--- U08/carrito/src/components/Header.jsx
+++ U08/carrito/src/components/Header.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header>
-      {/* Codigo de la cabecera de la web */}
+    <header style="background-color: #000000;">
+        <img class="logo" src="imagenes/GYM.png" alt="fondo de pagina"></img>
+        <div>
+
+            <nav aria-label="enlaces web" aria-current="index">
+                <div>
+                    <a class="enlace" href="index.html" style="color: #ffffff;">Home &nbsp &nbsp</a>
+                    <a class="enlace" href="paginas/nosotros.html" style="color: #ffffff;">Nosotros &nbsp &nbsp</a>
+                    <a class="enlace" href="paginas/productos.html" style="color: #ffffff;">Productos &nbsp &nbsp</a>
+                    <a class="enlace" href="paginas/cursos.html" style="color: #ffffff;">Cursos &nbsp &nbsp</a>
+                    <a class="enlace" href="paginas/subscripcion.html" style="color: #ffffff;">Subscripcion &nbsp&nbsp</a>
+                    <a class="enlace" href="paginas/contacto.html" style="color: #ffffff;">Contacto &nbsp &nbsp</a>
+                </div>
+            </nav>
+        </div>
     </header>
   );
 };
